1fdbcb40611c4922bed960180c902221ee2e88c6
WencoChen <1131691313@qq.com>
2019-08-13 09:28:38 +0800

    update

---
+++ Behavioral/Iterator/Book.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace DesignPatterns\Behavioral\Iterator;
+
+use Countable;
+use Iterator;
+
+class Book implements Iterator, Countable {
+
+
+    public function rewind()
+    {
+        // TODO: Implement rewind() method.
+    }
+
+
+
+}
